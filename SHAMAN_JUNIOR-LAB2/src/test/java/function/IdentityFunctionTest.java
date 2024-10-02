91face60aeba39ea58540a8bc8ca056636713b72
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2024-10-02 21:57:18 +0400

    Add Test

--- SHAMAN_JUNIOR-LAB2/src/test/java/function/IdentityFunctionTest.java
+++ SHAMAN_JUNIOR-LAB2/src/test/java/function/IdentityFunctionTest.java
@@ -1,8 +1,8 @@
 package function;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 class IdentityFunctionTest {
 
@@ -11,8 +11,9 @@ class IdentityFunctionTest {
 
         IdentityFunction obj = new IdentityFunction();
 
-        double res = obj.apply(2);
-
-        Assertions.assertEquals(2,res);
+        assertEquals(2,obj.apply(2));
+        assertEquals(3,obj.apply(3));
+        assertEquals(2.1,obj.apply(2.1));
+        assertEquals(-11111,obj.apply(-11111));
     }
 }
