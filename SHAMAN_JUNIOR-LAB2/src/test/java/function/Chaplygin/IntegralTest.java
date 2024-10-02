8fa42c614283b431de0d5e36374bc920d278ede6
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2024-10-02 21:48:54 +0400

    Add Tests

--- SHAMAN_JUNIOR-LAB2/src/test/java/function/Chaplygin/IntegralTest.java
+++ SHAMAN_JUNIOR-LAB2/src/test/java/function/Chaplygin/IntegralTest.java
@@ -9,11 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class IntegralTest {
 
     @Test
-    void solve() {
+    void solveByTrapezoidalRule() {
+        DoubleFunction<Double> f0 = (x) -> x*x;
+        DoubleFunction<Double> f1 = (x) -> Math.pow(Math.E,x);
+        DoubleFunction<Double> f2 = (x) -> x*x*x + x*2;
 
-//        DoubleFunction<Double> func = (x) -> Math.pow(Math.E,x);
-//        Integral object  = new Integral(func);
-//        assertEquals(330.6, object.solve(10,0));
+        Integral integral0 = new Integral(f0);
+        Integral integral1 = new Integral(f1);
+        Integral integral2 = new Integral(f2);
+
+        assertEquals(2673.72,integral0.solveByTrapezoidalRule(20,2),0.1);
+        assertEquals(23831.041,integral1.solveByTrapezoidalRule(10,0),0.1);
+        assertEquals(0,integral2.solveByTrapezoidalRule(10,-10),0.1);
+
 
     }
 }
