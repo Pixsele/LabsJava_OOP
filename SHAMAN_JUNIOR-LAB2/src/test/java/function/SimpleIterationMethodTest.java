a529a1874dc47f4bc1e022362ae660f6599802cc
Sofa <sguniceva45@gmail.com>
2024-10-09 22:41:00 +0400

    add test SimpleIterationMethod

--- SHAMAN_JUNIOR-LAB2/src/test/java/function/SimpleIterationMethodTest.java
+++ SHAMAN_JUNIOR-LAB2/src/test/java/function/SimpleIterationMethodTest.java
@@ -19,20 +19,43 @@ public class SimpleIterationMethodTest {
         assertEquals(1.25, res, 1e-6);
     }
 
-   /* @Test
-    public void TestingASistemOfEquations() {
-        // Уравнения
-        //x1 = (5*x2+3)/2
-        //x2 =(x1-1)/3
-        MathFunction equation1 = x -> (x + 3) / 2;
-        MathFunction equation2 = x -> (x - 1) / 3;
+    @Test
+    public void testSystemOfEquations() {
+        // Система уравнений:
+        // x1 = (x2 + 2) / 3
+        // x2 = (x1 + 1) / 2
+        MathFunction equation1 = x -> (x + 2) / 3;
+        MathFunction equation2 = x -> (x + 1) / 2;
 
-        MathFunction[] equations = new MathFunction[]{equation1,equation2};
+        MathFunction[] equations = new MathFunction[]{equation1, equation2};
         SimpleIterationMethod iterationMethod = new SimpleIterationMethod(equations, 1e-9, 100);
 
         double initialGuess = 0;
-        double res = iterationMethod.apply(initialGuess);
+        double root = iterationMethod.apply(initialGuess);
 
-        assertTrue(Math.abs(res - 1.6) < 1e-9 || Math.abs(res - 0.2) < 1e-9);
-    }*/
+        assertTrue(Math.abs(root - 1.0) < 1e-9 || Math.abs(root - 1.5) < 1e-9);
+    }
+
+    @Test
+    public void testNotCovergence(){
+        //ур-ние, которое не сходится
+        MathFunction equation = x -> 4 * x; // x=4x не имеет корней
+        MathFunction[] equations = new MathFunction[]{equation};
+        SimpleIterationMethod iterationMethod = new SimpleIterationMethod(equations, 1e-9, 100);
+
+        assertThrows(IllegalStateException.class, () -> iterationMethod.apply((1.0)));
+    }
+
+    @Test
+    public void testResultKnown(){
+        //yp-ние, результат которого известен
+        MathFunction equation = x -> x;
+        MathFunction[] equations = new MathFunction[]{equation};
+        SimpleIterationMethod iterationMethod = new SimpleIterationMethod(equations, 1e-9, 100);
+
+        double initialGuess = 1.0;
+        double root = iterationMethod.apply(initialGuess);
+
+        assertEquals(1.0, root, 1e-9);
+    }
 }
