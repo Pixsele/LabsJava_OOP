a150e2d9826c12946ebfbcc723548cf623457884
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2024-10-14 22:33:42 +0400

    Add TabulatedFunctionFileOutputStream

--- SHAMAN_JUNIOR-LAB2/src/main/java/io/TabulatedFunctionFileOutputStream.java
+++ SHAMAN_JUNIOR-LAB2/src/main/java/io/TabulatedFunctionFileOutputStream.java
@@ -1,9 +1,13 @@
 package io;
 
+import function.ArrayTabulatedFunction;
+import function.LinkedListTabulatedFunction;
+
 import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static io.FunctionsIO.writeTabulatedFunction;
 
 public class TabulatedFunctionFileOutputStream {
     public static void main(String[] args) {
@@ -13,7 +17,11 @@ public class TabulatedFunctionFileOutputStream {
             BufferedOutputStream bufferedOutputStream1 = new BufferedOutputStream(outputStream1);
             BufferedOutputStream bufferedOutputStream2 = new BufferedOutputStream(outputStream2);
 
+            ArrayTabulatedFunction arrayFunction = new ArrayTabulatedFunction(new double[]{1.0, 2.0, 3.0}, new double[]{2.0, 4.0, 6.0});
+            LinkedListTabulatedFunction linkedListFunction = new LinkedListTabulatedFunction(new double[]{1.0, 2.0, 3.0}, new double[]{3.0, 6.0, 9.0});
 
+            writeTabulatedFunction(bufferedOutputStream1,arrayFunction);
+            writeTabulatedFunction(bufferedOutputStream2,linkedListFunction);
 
         } catch (IOException e) {
             e.printStackTrace();
