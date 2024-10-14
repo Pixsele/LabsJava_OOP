2b1b45be460c6fb0df92db4fc3432256661e540e
Sofa <sguniceva45@gmail.com>
2024-10-14 22:35:17 +0400

    add new method

--- SHAMAN_JUNIOR-LAB2/src/main/java/io/FunctionsIO.java
+++ SHAMAN_JUNIOR-LAB2/src/main/java/io/FunctionsIO.java
@@ -1,7 +1,28 @@
 package io;
 
+import function.api.TabulatedFunction;
+import function.Point;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO (){
         throw new UnsupportedOperationException();
+    }
+
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+        PrintWriter printWriter = new PrintWriter(writer);
+        // Записываем количество точек
+        printWriter.println(function.getCount());
+
+        // Записываем каждую точку
+        for (Point point : function) {
+            printWriter.printf("%f %f%n", point.x, point.y);
+        }
+
+        // Пробрасываем данные из буфера в поток
+        printWriter.flush();
     }
 }
