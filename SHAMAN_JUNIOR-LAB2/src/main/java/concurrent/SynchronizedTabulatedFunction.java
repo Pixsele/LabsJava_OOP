5553224997070555dca7d7614c1b98e9879d0ecb
Sofa <sguniceva45@gmail.com>
2024-10-22 18:14:31 +0400

    add interface Operation and method doSynchronously

--- SHAMAN_JUNIOR-LAB2/src/main/java/concurrent/SynchronizedTabulatedFunction.java
+++ SHAMAN_JUNIOR-LAB2/src/main/java/concurrent/SynchronizedTabulatedFunction.java
@@ -105,5 +105,14 @@ public class SynchronizedTabulatedFunction implements TabulatedFunction {
 
         }
     }
+    public interface Operation<T> {
+        T apply(SynchronizedTabulatedFunction function);
+    }
+
+    public <T> T doSynchronously(Operation<? extends T> operation) {
+        synchronized (function) {
+            return operation.apply(this);
+        }
+    }
 }
 
