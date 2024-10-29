36e1a2a9b6a48ba1d1c2afba40d1bee9658a9bbf
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2024-10-29 18:15:13 +0400

    Update Pom, create application.properties and sql scripts

---
+++ SHAMAN_JUNIOR-LAB2/src/main/resources/sql/cleanup.sql
@@ -0,0 +1,4 @@
+-- Очищаем таблицы в обратном порядке зависимости
+DELETE FROM parameters;
+DELETE FROM resultsByParametr;
+DELETE FROM functions;
---
+++ SHAMAN_JUNIOR-LAB2/src/main/resources/sql/data.sql
@@ -0,0 +1,4 @@
+-- Заполняем таблицу functions
+INSERT INTO functions (id, function_name, function_type, creation_time) VALUES
+(1, 'Linear', 'Basic', NOW()),
+(2, 'Quadratic', 'Composite', NOW());
